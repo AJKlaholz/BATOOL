da8c232e3f7231485c6b23d7ecde0b2ee6ac34fd
PC_AJKlaholz <adrian.klaholz@gmail.com>
2017-02-08 19:43:23 +0100

    e

--- TestWithBash/src/application/boundary/LineChart_AWT.java
+++ TestWithBash/src/application/boundary/LineChart_AWT.java
@@ -51,7 +51,8 @@ public class LineChart_AWT extends ApplicationFrame {
 	public LineChart_AWT(String applicationTitle, String chartTitle, String r, OpenFile of, JColorComboBox[] box) {
 		super(applicationTitle);
 		JFreeChart lineChart = ChartFactory.createTimeSeriesChart(chartTitle, "time interval", "popularity",
-				createDataset(GPGTrends.parsDataFromJavaIntoRecord(rm.getRecord(r)), Excel.ExceltoJava(of)), true, true, false);
+				createDataset(GPGTrends.parsDataFromJavaIntoRecord(rm.getRecord(r)), Excel.ExceltoJava(of)), true, true,
+				false);
 
 		ChartPanel chartPanel = new ChartPanel(lineChart);
 		chartPanel.setPreferredSize(new java.awt.Dimension(1500, 1000));
@@ -62,10 +63,9 @@ public class LineChart_AWT extends ApplicationFrame {
 		for (int i = 0; i < rm.getRecord(r).getListOfSTerm().size(); i++) {
 			renderer.setSeriesPaint(i, box[i].getSelectedColor());
 		}
-		renderer.setSeriesPaint(rm.getRecord(r).getListOfSTerm().size(), box[box.length-1].getSelectedColor());
-		//renderer.setSeriesPaint(rm.getRecord(r).getListOfSTerm().size(),box[rm.getRecord(r).getListOfSTerm().size()].getSelectedColor());
-		
-		
+		renderer.setSeriesPaint(rm.getRecord(r).getListOfSTerm().size(), box[box.length - 1].getSelectedColor());
+		// renderer.setSeriesPaint(rm.getRecord(r).getListOfSTerm().size(),box[rm.getRecord(r).getListOfSTerm().size()].getSelectedColor());
+
 		System.out.println(box.length);
 		plot.setRenderer(renderer);
 		setContentPane(chartPanel);
@@ -81,26 +81,25 @@ public class LineChart_AWT extends ApplicationFrame {
 			for (Entry<Calendar, Double> entry : gt.getListOfSTerm().get(i).getDateListFromSearchterm().entrySet()) {
 
 				for (Entry<Calendar, Double> product : product2.getOrderDRequest().entrySet()) {
-			
-						if(entry.getKey().equals(product.getKey())){
-							a.add(entry.getValue());
-							b.add(product.getValue());
-							
-						}
+
+					if (entry.getKey().equals(product.getKey())) {
+						a.add(entry.getValue());
+						b.add(product.getValue());
+
+					}
 				}
-				
+
 				xys.addOrUpdate((new Day(entry.getKey().get(Calendar.DAY_OF_MONTH),
 						(entry.getKey().get(Calendar.MONTH)) + 1, entry.getKey().get(Calendar.YEAR))),
-						entry.getValue());					
-			
-			
+						entry.getValue());
+
 			}
-				
-			
-			xys.setKey(gt.getListOfSTerm().get(i).getName()+"("+ roundScale2(Correlation(a.toArray(new Double[a.size()]), b.toArray(new Double[a.size()])) )+")");
+
+			xys.setKey(gt.getListOfSTerm().get(i).getName() + "("
+					+ roundScale2(Correlation(a.toArray(new Double[a.size()]), b.toArray(new Double[a.size()]))) + ")");
 			dataset.addSeries(xys);
 
-	}
+		}
 		// TimeSeries pro = new TimeSeries(Excel.ExceltoJava().getName());
 		TimeSeries pro = new TimeSeries(product2.getName());
 		for (Entry<Calendar, Double> entry : product2.getOrderDRequest().entrySet()) {
@@ -108,46 +107,45 @@ public class LineChart_AWT extends ApplicationFrame {
 					(entry.getKey().get(Calendar.MONTH)) + 1, entry.getKey().get(Calendar.YEAR))), entry.getValue());
 		}
 		dataset.addSeries(pro);
-		
+
 		return dataset;
 	}
+
 	public static double Correlation(Double[] doubles, Double[] doubles2) {
-	    //TODO: check here that arrays are not null, of the same length etc
+		// TODO: check here that arrays are not null, of the same length etc
 
-	    double sx = 0.0;
-	    double sy = 0.0;
-	    double sxx = 0.0;
-	    double syy = 0.0;
-	    double sxy = 0.0;
+		double sx = 0.0;
+		double sy = 0.0;
+		double sxx = 0.0;
+		double syy = 0.0;
+		double sxy = 0.0;
 
-	    int n = doubles.length;
+		int n = doubles.length;
 
-	    for(int i = 0; i < n; ++i) {
-	      double x = doubles[i];
-	      double y = doubles2[i];
+		for (int i = 0; i < n; ++i) {
+			double x = doubles[i];
+			double y = doubles2[i];
 
-	      sx += x;
-	      sy += y;
-	      sxx += x * x;
-	      syy += y * y;
-	      sxy += x * y;
-	    }
+			sx += x;
+			sy += y;
+			sxx += x * x;
+			syy += y * y;
+			sxy += x * y;
+		}
 
-	    // covariation
-	    double cov = sxy / n - sx * sy / n / n;
-	    // standard error of x
-	    double sigmax = Math.sqrt(sxx / n -  sx * sx / n / n);
-	    // standard error of y
-	    double sigmay = Math.sqrt(syy / n -  sy * sy / n / n);
+		// covariation
+		double cov = sxy / n - sx * sy / n / n;
+		// standard error of x
+		double sigmax = Math.sqrt(sxx / n - sx * sx / n / n);
+		// standard error of y
+		double sigmay = Math.sqrt(syy / n - sy * sy / n / n);
 
-	    // correlation is just a normalized covariation
-	    return cov / sigmax / sigmay;
-	  }
-	static double roundScale2( double d )
-	  {
-	    return Math.rint( d * 1000 ) / 1000.;
-	  }
-	
-	
+		// correlation is just a normalized covariation
+		return cov / sigmax / sigmay;
+	}
+
+	static double roundScale2(double d) {
+		return Math.rint(d * 1000) / 1000.;
+	}
 
 }
